fdb0214e672d5187787a726f0539b757d636fdd4
MickaelD <mickaelducelier@hotmail.fr>
2019-01-07 15:17:15 +0100

    Update Pool and application.properties

--- monitrackService/src/main/java/com/monitrack/connectionPool/implementation/JDBCConnectionPool.java
+++ monitrackService/src/main/java/com/monitrack/connectionPool/implementation/JDBCConnectionPool.java
@@ -1,29 +1,67 @@
 package com.monitrack.connectionPool.implementation;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Vector;
 
 import com.monitrack.connectionPool.interfaces.IJDBCConnectionPool;
+import com.monitrack.util.Util;
 
 public class JDBCConnectionPool implements IJDBCConnectionPool {
-
-	/**
-	 * Attribut collectionnant les connexions physiques instances de la classe connexion
-	 */
+	
+    private Vector<Connection> pool;
+    private static final String URL             = "//192.168.1.1:5432/moniTrack";
+    private static final String DRIVER          =  Util.getPropertyValueFromPropertiesFile("driver");
+    private static final String USER            =  Util.getPropertyValueFromPropertiesFile("username_dev");
+    private static final String PSWD            =  Util.getPropertyValueFromPropertiesFile("password_dev");
 	private ArrayList<Connection> connections;
 	
 	public JDBCConnectionPool() {
 		// TODO : créer l'instance de l'attribut connections
 	}
+	
 
 	public void fillConnectionsList(int numberOfConnections) {
-		// TODO Auto-generated method stub
-		
+        pool = new Vector<Connection>();
+        for (int i=0; i<numberOfConnections; i++ ){
+            pool.addElement(this.getConnection());
+        }
 	}
 
 	public Connection getConnection() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("-------- MySQL "
+                + "JDBC Connection Testing ------------");
+        try {
+            Class.forName("org.mysql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver? "
+                    + "Include in your library path!");
+            e.printStackTrace();
+            
+        }
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+        try {
+            String lien = "jdbc:"+ DRIVER+":"+ URL;
+            connection = DriverManager.getConnection(
+                    lien, USER,
+                    PSWD);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return null;
+        }
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+            return connection;
+        } else {
+            System.out.println("Failed to make connection!");
+            return null;
+        }
+        
+
 	}
 
 	public void putConnection(Connection connection) {
@@ -35,6 +73,18 @@ public class JDBCConnectionPool implements IJDBCConnectionPool {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+    public Connection giveCo(){
+        Connection togive = (Connection) pool.lastElement();
+        pool.removeElementAt(pool.indexOf(togive));
+        return togive;
+    }
+    
+    
+    public void getBackCo(Connection co){
+        pool.addElement(co);
+    }
+    
 
 	
 
