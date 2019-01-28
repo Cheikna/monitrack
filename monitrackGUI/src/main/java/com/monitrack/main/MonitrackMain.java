186fcdf5aabbe596283afdd0abbc3552d1ba6682
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-01-28 20:52:49 +0100

    Intégration de slf4j
    slf4j est fonctionnel et permet d'écrire des logs sur la console et dans
    un fichier monitrack.log (créé au même endroit que l'emplacement de
    lancement du .jar de monitrack). Il est ainsi possible d'avoir un
    fichier .log qui pourra contenir les différentes traces que l'on voudra.

--- monitrackGUI/src/main/java/com/monitrack/main/MonitrackMain.java
+++ monitrackGUI/src/main/java/com/monitrack/main/MonitrackMain.java
@@ -1,10 +1,16 @@
 package com.monitrack.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.monitrack.gui.frame.MonitrackFrame;
 
 public class MonitrackMain {
 
+	private static Logger log = LoggerFactory.getLogger(MonitrackMain.class);
+	
 	public static void main(String[] args) {
+		log.info("Lancement de Monitrack");
 		MonitrackFrame monitrack = new MonitrackFrame();
 	}
 
