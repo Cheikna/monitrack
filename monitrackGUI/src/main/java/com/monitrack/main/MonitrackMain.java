274d055ec9e9e240c7f9879cf7c0b60ab0a3376c
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-02-10 13:48:58 +0100

    Ajout d'un Thread lors de l'appel de la méthode main()

--- monitrackGUI/src/main/java/com/monitrack/main/MonitrackMain.java
+++ monitrackGUI/src/main/java/com/monitrack/main/MonitrackMain.java
@@ -1,17 +1,28 @@
 package com.monitrack.main;
 
+import javax.swing.SwingUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.monitrack.gui.frame.MonitrackFrame;
 
 public class MonitrackMain {
 
 	private static Logger log = LoggerFactory.getLogger(MonitrackMain.class);
-	
-	public static void main(String[] args) {
-		log.info("Lancement de Monitrack");
-		MonitrackFrame monitrack = new MonitrackFrame();
-	}
 
+	public static void main(String[] args) {		
+		/*
+		 * The swing objects are not thread-safe which means that some errors can happens when we are accessing to the same data
+		 * In order to prevent this from happening, we have to use a special Thread called 'SwingUtilities.invokeLater'
+		 */
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@SuppressWarnings("unused")
+			public void run() {
+				log.info("Launching of Monitrack");
+				MonitrackFrame monitrack = new MonitrackFrame();			
+			}
+		});
+
+	}
+	
 }
