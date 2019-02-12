4403e11b16c63a75e55ec03264ed83cb1f66f081
taoudiatguillaume <taoudiat.guillaume@gmail.com>
2019-02-12 10:15:07 +0100

    Séparation de la classe MonitrackFrame en 3 sous classes.

---
+++ monitrackGUI/src/main/java/com/monitrack/listener/HomePageListener.java
@@ -0,0 +1,54 @@
+package com.monitrack.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import com.monitrack.dao.implementation.PersonDAO;
+import com.monitrack.entity.Person;
+import com.monitrack.gui.frame.MonitrackFrame;
+import com.monitrack.gui.panel.HomePage;
+
+public class HomePageListener implements ActionListener
+{
+	private HomePage homePage;
+	// DAO for request
+	private PersonDAO personDAO = new PersonDAO();
+	public HomePageListener(HomePage homePage)
+	{
+		this.homePage = homePage;
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()== homePage.getJbValidate())
+		{
+			String name = homePage.getJtfName().getText().trim();
+			if(name.length() <= 0)
+			{
+				JOptionPane.showMessageDialog(null, "Nom incorrect");
+			}
+			else
+			{
+				Date sqlCurrentDate = new Date(Calendar.getInstance().getTime().getTime());
+				Person person = new Person(name, sqlCurrentDate);
+				personDAO.create(person);
+			}
+		}
+		if(e.getSource()==homePage.getJbOverview())
+		{
+			List<Person> persons = personDAO.findAll();
+			String personsText = "";
+			for(Person person : persons)
+			{
+				personsText += person.getIdPerson() + ") " + person.getNamePerson() + " ---- " + person.getCreateDate() + "\n";
+			}
+			homePage.getjTArea().setText(personsText);
+		}
+	}
+
+
+}
