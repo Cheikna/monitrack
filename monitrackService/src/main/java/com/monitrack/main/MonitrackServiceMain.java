27e9681840be66a407a524b9c434681ea3b44f1b
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-02-28 14:04:35 +0100

    Amélioration de la méthode main du module monitrackService

--- monitrackService/src/main/java/com/monitrack/main/MonitrackServiceMain.java
+++ monitrackService/src/main/java/com/monitrack/main/MonitrackServiceMain.java
@@ -15,17 +15,21 @@ public class MonitrackServiceMain {
 		for(int i = 1; i <= tooMuchConnections; i++) {
 			System.out.println("-------------------------------- Loop n°" + i + "/" + tooMuchConnections + " --------------------------------");
 			connection = DataSource.getConnection();
+			
+			/*
+			 * We check "i > numberOfConnections" because i starts at 1 and not at 0
+			 */
+			if(i > numberOfConnections)
+			{
+				System.out.println("We are try to access to the connection n°" + i + " whereas there were only " + numberOfConnections + " available at the beginning on the DataSource.");
+				System.out.println("Normally, the next connection should be null !");
+			}
 			if(connection != null)
 			{
 				System.out.println("The connection n°" + i + " from the DataSource is not null.");
 			}
 			else
-			{
-				if(i > numberOfConnections)
-				{
-					System.out.println("We are try to access to the connection n°" + i + " whereas there were only " + numberOfConnections + " available at the beginning on the DataSource.");
-					System.out.println("Normally, the next connection should be null !");
-				}
+			{				
 				System.out.println("The connection n°" + i + " from the DataSource is null.");
 				System.out.println("------------------------------- End : Results -------------------------------");
 				System.out.println("Number of connections in the DataSource : " + numberOfConnections);
