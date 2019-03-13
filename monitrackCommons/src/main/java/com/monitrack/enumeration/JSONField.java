ed740a3f1774193d890425abe311daa8d7efcbad
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-03-13 21:14:35 +0100

    Ajout d'une énumération pour les champs JSON

---
+++ monitrackCommons/src/main/java/com/monitrack/enumeration/JSONField.java
@@ -0,0 +1,36 @@
+package com.monitrack.enumeration;
+
+public enum JSONField {
+	
+	ENTITY("entity"),
+	
+	DATAS("datas"),
+	
+	IS_LIST("is_list_of_entities"),
+	
+	REQUEST_INFO("request_info"),
+	
+	REQUEST_TYPE("request_type"),
+	
+	REQUESTED_FIELDS("requested_fields"),
+	
+	REQUESTED_VALUES("requested_values"),
+	
+	REQUESTED_ENTITY("requested_entity"),
+	
+	SERIALIZED_OBJECT("serialized_object");
+	
+	
+	private String label;
+	
+	JSONField(String label)
+	{
+		this.label = label;
+	}
+	
+	public String getLabel()
+	{
+		return this.label;
+	}
+
+}
