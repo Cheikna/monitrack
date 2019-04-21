d7ceea1789bba4bd3db39789e6ad392a21884d54
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-04-21 13:26:39 +0200

    DAO pour la classe mère : SensorDAO

--- monitrack-service/src/main/java/com/monitrack/dao/implementation/SensorDAO.java
+++ monitrack-service/src/main/java/com/monitrack/dao/implementation/SensorDAO.java
@@ -4,22 +4,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.monitrack.dao.abstracts.DAO;
-import com.monitrack.entity.Flow;
 import com.monitrack.entity.Sensor;
+import com.monitrack.entity.Location;
 import com.monitrack.enumeration.SensorActivity;
 import com.monitrack.enumeration.SensorType;
 
-public abstract class SensorDAO<T> extends DAO<T> {
+public class SensorDAO<T> extends DAO<T> {
 	
 	private static final Logger log = LoggerFactory.getLogger(SensorDAO.class);
 	private final Object lock = new Object();	
+	private static String MOTHER_TABLE_NAME = "SENSOR";
 	private String tableName;
 
 	public SensorDAO(Connection connection, String tableName) {
@@ -27,14 +30,60 @@ public abstract class SensorDAO<T> extends DAO<T> {
 		this.tableName = tableName;
 	}
 
-	public int createSensor(T obj) {
-		//FIXME
-		int id = 1;
-		return id;
+	public SensorDAO(Connection connection) {
+		super(connection);
+		this.tableName = MOTHER_TABLE_NAME;
 	}
 
-	public void updateSensor(T obj) {
-		//FIXME
+	@Override
+	public T create(T obj) {
+		synchronized (lock) {
+			// Checks if the connection is not null before using it
+			if (obj instanceof Sensor && connection != null) {
+				Sensor sensor = (Sensor)obj;
+				try {
+					PreparedStatement preparedStatement = connection
+							.prepareStatement("INSERT INTO ? (TYPE, ACTIVITY, ID_LOCATION, IP_ADDRESS, MAC_ADDRESS, "
+															+ "SERIAL_NUMBER, HARDWARE_VERSION, SOFTWARE_VERSION, "
+															+ " START_ACTIVITY_TIME, END_ACTIVITY_TIME, CHECK_FREQUENCY, "
+															+ "MEASUREMENT_UNIT, CURRENT_DANGER_THRESHOLD, POSITION_X, POSITION_Y) "
+															+ "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ", Statement.RETURN_GENERATED_KEYS);
+					preparedStatement.setString(1, MOTHER_TABLE_NAME);
+					preparedStatement.setString(2, sensor.getSensorType().toString());
+					preparedStatement.setString(3, sensor.getSensorActivity().toString());
+					preparedStatement.setInt(4, sensor.getLocationId());
+					preparedStatement.setString(5, sensor.getIpAddress());
+					preparedStatement.setString(6, sensor.getMacAddress());
+					preparedStatement.setString(7, sensor.getSerialNumber());
+					preparedStatement.setFloat(8, sensor.getHardwareVersion());
+					preparedStatement.setFloat(9, sensor.getSoftwareVersion());
+					preparedStatement.setTime(10, sensor.getBeginTime());
+					preparedStatement.setTime(11, sensor.getEndTime());
+					preparedStatement.setFloat(12, sensor.getCheckFrequency());
+					preparedStatement.setString(13, sensor.getMeasurementUnit());
+					preparedStatement.setFloat(14, sensor.getDangerThreshold());
+					preparedStatement.setFloat(15, sensor.getPositionX());
+					preparedStatement.setFloat(16, sensor.getPositionY());
+					preparedStatement.execute();
+					ResultSet rs = preparedStatement.getGeneratedKeys();
+					int lastCreatedId = 0;
+					if (rs.next()) {
+						lastCreatedId = rs.getInt(1);
+						sensor.setId(lastCreatedId);
+					}
+				} catch (Exception e) {
+					log.error("An error occurred during the creation of a location : " + e.getMessage());
+					e.printStackTrace();
+				}
+				return (T)sensor;
+			}
+			return obj;
+		}
+	}
+
+	@Override
+	public void update(T obj) {
+		// FIXME Auto-generated method stub
 		
 	}
 	
@@ -57,9 +106,15 @@ public abstract class SensorDAO<T> extends DAO<T> {
 		}		
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<T> find(List<String> fields, List<String> values) {
 		synchronized (lock) {
-			String sql = "select * from " + tableName +" first_table inner join SENSOR second_table on first_table.ID_SENSOR = second_table.ID_SENSOR";
+			String sql = "";
+			if(!tableName.equalsIgnoreCase(MOTHER_TABLE_NAME))
+				sql = "select * from " + tableName +" first_table inner join SENSOR second_table on first_table.ID_SENSOR = second_table.ID_SENSOR";
+			else
+				sql = "select * from " + MOTHER_TABLE_NAME;
+			
 			List<T> sensors = new ArrayList<T>();
 			sql += super.getRequestFilters(fields, values);
 			if (connection != null) {
@@ -82,6 +137,7 @@ public abstract class SensorDAO<T> extends DAO<T> {
 		}
 	}
 	
+	@SuppressWarnings("finally")
 	protected Sensor getSensorFromResultSet(ResultSet rs) {
 		Sensor sensor = null;
 		try {
@@ -91,13 +147,22 @@ public abstract class SensorDAO<T> extends DAO<T> {
 					rs.getTimestamp("CREATION_DATE"), rs.getTimestamp("LAST_MESSAGE_DATE"), rs.getTimestamp("LAST_CONFIGURATION_DATE"),
 					rs.getTime("START_ACTIVITY_TIME"),rs.getTime("END_ACTIVITY_TIME"),rs.getFloat("CHECK_FREQUENCY"),
 					rs.getString("MEASUREMENT_UNIT"),rs.getFloat("CURRENT_DANGER_THRESHOLD"),rs.getFloat("POSITION_X"),rs.getFloat("POSITION_Y"));
+			
+			sensor.setLocation(getSensorLocation(rs.getInt("ID_LOCATION")));
+			
+			
 		} catch (SQLException e) {
 			log.error("An error occurred when getting one Flow Sensor from the resultSet : " + e.getMessage());
 		}
 		finally {
 			return sensor;
 		}
-		
+	}
+	
+	protected Location getSensorLocation(Integer sensorId) {
+		LocationDAO locationDAO = new LocationDAO(connection);
+		Location location = locationDAO.find(Arrays.asList("ID_LOCATION"), Arrays.asList(sensorId.toString())).get(0);
+		return location;
 	}
 
 }
