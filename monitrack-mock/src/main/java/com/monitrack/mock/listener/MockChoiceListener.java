9c050b81c6f717292f77c3ebe15ed4627deff324
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-04-24 11:02:10 +0200

    Ajout d'un page pour la génération de capteurs

--- monitrack-mock/src/main/java/com/monitrack/mock/listener/MockChoiceListener.java
+++ monitrack-mock/src/main/java/com/monitrack/mock/listener/MockChoiceListener.java
@@ -23,8 +23,10 @@ public class MockChoiceListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == mockChoicePage.getSensorOverviewButton()) {
-			parentFrame.changePage(Page.SENSOR_OVERVIEW);
-			
+			parentFrame.changePage(Page.SENSOR_OVERVIEW);			
+		}
+		else if(e.getSource() == mockChoicePage.getGenerateRandomSensorButton()) {
+			parentFrame.changePage(Page.SENSORS_FACTORY);			
 		}
 
 	}
