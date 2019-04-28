6db57d36234f96b5f7fbed019758a3a1b5d65332
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-04-28 17:22:44 +0200

    Vérification de la présence d'un capteur dans le cache

--- monitrack-service/src/main/java/com/monitrack/data/pool/DataPool.java
+++ monitrack-service/src/main/java/com/monitrack/data/pool/DataPool.java
@@ -15,12 +15,14 @@ import org.slf4j.LoggerFactory;
 
 import com.monitrack.connection.pool.implementation.DataSource;
 import com.monitrack.dao.implementation.DAOFactory;
-import com.monitrack.dao.implementation.SensorDAO;
+import com.monitrack.dao.implementation.SensorConfigurationDAO;
 import com.monitrack.entity.Message;
-import com.monitrack.entity.Sensor;
+import com.monitrack.entity.SensorConfiguration;
+import com.monitrack.entity.SensorConfigurationHistory;
 import com.monitrack.enumeration.RequestType;
 import com.monitrack.enumeration.SensorActivity;
 import com.monitrack.enumeration.SensorState;
+import com.monitrack.enumeration.SensorType;
 import com.monitrack.util.Util;
 
 public class DataPool {
@@ -33,27 +35,19 @@ public class DataPool {
 	private final List<String> fieldsForActiveSensors = Arrays.asList("ACTIVITY");
 	private final List<String> valuesForActiveSensors = Arrays.asList(SensorActivity.ENABLED.toString());
 
-	private Map<Sensor, SensorState> dataPoolCache;
-	private List<Sensor> activeSensors;	
+	private Map<SensorConfiguration, SensorState> dataPoolCache;
+	private List<SensorConfiguration> activeSensors;	
 	private Timestamp currentTime;
 	private Thread listUpdaterThread;
 	private long counter;
 	private Connection connection;
 
-	public DataPool() {
-		connection = DataSource.getConnection();
-		Sensor sensor = (Sensor) new SensorDAO(connection).find(Arrays.asList("ID_SENSOR"), Arrays.asList("8")).get(0);
-		Sensor sensor2 = (Sensor) new SensorDAO(connection).find(Arrays.asList("ID_SENSOR"), Arrays.asList("10")).get(0);
-		DataSource.putConnection(connection);
-		dataPoolCache = Collections.synchronizedMap(new HashMap<Sensor, SensorState>());
-		dataPoolCache.put(sensor, SensorState.DANGER);
-		dataPoolCache.put(sensor2, SensorState.NORMAL);
-		activeSensors = Collections.synchronizedList(new ArrayList<Sensor>());
-		System.out.println(sensor);
-		System.out.println(sensor2);
+	public DataPool() {		
+		dataPoolCache = Collections.synchronizedMap(new HashMap<SensorConfiguration, SensorState>());
+		activeSensors = Collections.synchronizedList(new ArrayList<SensorConfiguration>());
 		counter = updateListFrequency;
 	}
-	
+
 	/**
 	 * Starts the update list thread to that the data can be refresh every 10 seconds
 	 */
@@ -80,10 +74,10 @@ public class DataPool {
 		listUpdaterThread.start();
 	}
 
-	public synchronized List<Sensor> getCacheSensorsByState(SensorState sensorState) {
-		List<Sensor> results = new ArrayList<Sensor>();		
+	public synchronized List<SensorConfiguration> getCacheSensorsByState(SensorState sensorState) {
+		List<SensorConfiguration> results = new ArrayList<SensorConfiguration>();		
 
-		for(Map.Entry<Sensor, SensorState > mapEntry : dataPoolCache.entrySet()) {
+		for(Map.Entry<SensorConfiguration, SensorState > mapEntry : dataPoolCache.entrySet()) {
 			if(mapEntry.getValue() == sensorState) {
 				results.add(mapEntry.getKey());
 			}
@@ -93,15 +87,80 @@ public class DataPool {
 	}
 
 	public synchronized void processMessage(Message receivedMessage) {
-		currentTime = new Timestamp(System.currentTimeMillis());
-		Sensor sensor = receivedMessage.getSensor();
-		System.err.println("=========> Sensor : " + sensor.getSensorType());
-		//System.err.println("=========> LEVEL : " + sensor.getCurrentThreshold() + "/" + sensor.getMaxDangerThreshold());
-		System.out.println("==========> State : " + receivedMessage.getSensorState());
-		if(sensor.raiseDangerAlert())
-			System.err.println("/!\\ DANGER DANGER /!\\");
+		try {
+			log.info("Processing sensor message");
+			currentTime = new Timestamp(System.currentTimeMillis());
+			boolean isSensorWithCorrectIntervalInCache = false;
+			SensorConfiguration sensorFromCache = null;
+			SensorState sensorStateFromCache = null;
+			
+			SensorConfiguration sensorFromMessage = receivedMessage.getSensor();
+			SensorState sensorStateFromMessage = receivedMessage.getSensorState();
+			
+			log.info(sensorFromMessage.getStateInfo());
+			int sensorId = sensorFromMessage.getId();
+			
+			for (Map.Entry<SensorConfiguration, SensorState> mapEntry : dataPoolCache.entrySet()) {
+				if (mapEntry.getKey().equals(sensorFromMessage)) {
+					sensorFromCache = mapEntry.getKey();
+					sensorFromCache.setLastMessageDate();
+					sensorStateFromCache = mapEntry.getValue();
+					Long cacheTime = sensorFromCache.getLastMessageDate().getTime();
+					
+					if(sensorStateFromMessage == SensorState.NORMAL && sensorStateFromCache == SensorState.DANGER) {
+						SensorConfigurationHistory hist = new SensorConfigurationHistory();
+					}					
+					else if ((cacheTime != null) && (currentTime.getTime() - cacheTime) <= sensorFromCache.getCheckFrequency()) {
+						log.info("The sensor n°" + sensorId + " has been found in the cache");
+						//FIXME Cheikna (= ME) should see if the date is not already updated
+						//sensorFromCache.setLastMessageDate();
+						log.info("Old value : " + sensorFromCache.getStateInfo());
+
+						//FIXME put this time in a config file
+						Thread.sleep(1000);
+						
+						// FIXME Checks if the value has decreased and if it is the case, save the value
+						Float maxThreshold = sensorFromMessage.getMaxDangerThreshold();
+						Float minThreshold = sensorFromMessage.getMinDangerThreshold();
+						Float threshold = sensorFromMessage.getCurrentThreshold();
+								
+						if (threshold >= maxThreshold || threshold < minThreshold) {
+							sensorStateFromMessage = SensorState.DANGER;
+							//FIXME Save into the database because it is a danger
+						}						
+						else if(threshold < sensorFromCache.getCurrentThreshold()) {
+							//FIXME Save in the history
+							//FIXME Save the data in the database in order to save the pic
+						}
+						else if(sensorFromMessage.getCurrentThreshold() == 0) {
+							sensorStateFromMessage = SensorState.NORMAL;
+						}
+
+						isSensorWithCorrectIntervalInCache = true;
+					}
+					break;
+				}
+			}
+			if (!isSensorWithCorrectIntervalInCache) {
+				log.info("The sensor n°" + sensorId
+						+ " has not been found in the cache (or was old). It will be added (or updated) to it !");
+				sensorFromMessage.setLastMessageDate();
+			}
+			
+			//Inserts or updates the value in the cache
+			dataPoolCache.put(sensorFromMessage, sensorStateFromMessage);
+			
+		} catch (Exception e) {
+			log.error(e.getStackTrace().toString());
+			e.printStackTrace();
+		}
+
+		/*for(Map.Entry<Sensor, SensorState > mapEntry : dataPoolCache.entrySet()) {
+			System.out.println("In the cache, there is : " + mapEntry.getKey());
+		}*/
+
 		//updateActiveSensorsList();
-		//FIXME Récupérer tous les capteurs d'une même salle
+		//FIXME Récupérer tous les capteurs d'une même salle*/
 
 	}
 
@@ -110,7 +169,7 @@ public class DataPool {
 		try {			
 			connection = DataSource.getConnection();
 			activeSensors.clear();
-			activeSensors.addAll((List<Sensor>)DAOFactory.execute(connection, Sensor.class, RequestType.SELECT, null, fieldsForActiveSensors, valuesForActiveSensors));
+			activeSensors.addAll((List<SensorConfiguration>)DAOFactory.execute(connection, SensorConfiguration.class, RequestType.SELECT, null, fieldsForActiveSensors, valuesForActiveSensors));
 			DataSource.putConnection(connection);
 			connection = null;
 			log.info("active sensors list updated");
@@ -127,13 +186,13 @@ public class DataPool {
 	public void checkAllSensorsActivity() {
 		synchronized (lock) {
 			System.out.println("===> size : " + activeSensors.size());
-			for(Sensor sensor : activeSensors) {
-				for(Map.Entry<Sensor, SensorState> mapEntry : dataPoolCache.entrySet()) {
-					Sensor cacheSensor = mapEntry.getKey();
-					if(sensor.getId() == cacheSensor.getId())
-						System.out.println("The sensor with the id n°" + sensor.getId() + " is in the cache");
+			for(SensorConfiguration sensorConfiguration : activeSensors) {
+				for(Map.Entry<SensorConfiguration, SensorState> mapEntry : dataPoolCache.entrySet()) {
+					SensorConfiguration cacheSensor = mapEntry.getKey();
+					if(sensorConfiguration.getId() == cacheSensor.getId())
+						System.out.println("The sensor with the id n°" + sensorConfiguration.getId() + " is in the cache");
 					else
-						System.err.println("The sensor with the id n°" + sensor.getId() + " is not in the cache");	
+						System.err.println("The sensor with the id n°" + sensorConfiguration.getId() + " is not in the cache");	
 
 				}
 			}
@@ -141,8 +200,32 @@ public class DataPool {
 	}
 
 	public synchronized void displayUpdatedSensors() {
-		for(Sensor sensor : activeSensors)
-			System.out.println(sensor);
+		for(SensorConfiguration sensorConfiguration : activeSensors)
+			System.out.println(sensorConfiguration);
 	}
+	
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+		//Sensor sensor = (Sensor) new SensorDAO(connection).find(Arrays.asList("ID_SENSOR"), Arrays.asList("8")).get(0);
+		SensorConfiguration sensorConfiguration = new SensorConfiguration(8, 0, SensorActivity.ENABLED, SensorType.FLOW, 1, "192.168.20.15", "dsfsd", "dsfsdf", 
+				1.0f, 2.0f, null, null, null, null, null, 2500f, "Decibel", 4.0f, 0.0f, 5.0f, 6.23f, 4.94f);
+		sensorConfiguration.setLastMessageDate();
+		for(Integer i = 29; i < 36; i++) {
+			SensorConfiguration sensor2 = (SensorConfiguration) new SensorConfigurationDAO(connection).find(Arrays.asList("ID_SENSOR"), Arrays.asList(i.toString())).get(0);
+			dataPoolCache.put(sensor2, SensorState.DANGER);
+		}
+		
+		dataPoolCache.put(sensorConfiguration, SensorState.DANGER);
+	}
+	
+	private void processSensorMesage(Message message) {
+		//FIXME
+	}
+	
+	private void processManualTrigger(Message message) {
+		//FIXME
+	}
+	
+	
 
 }
