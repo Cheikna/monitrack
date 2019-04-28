0257bad8844d74f52e04bf4e505b4b05aab3b110
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-04-28 14:24:16 +0200

    Fusion de type de capteurs # Conflicts: #	monitrack-commons/src/main/java/com/monitrack/entity/Sensor.java #	monitrack-commons/src/main/java/com/monitrack/enumeration/SensorType.java

--- monitrack-commons/src/main/java/com/monitrack/enumeration/SensorType.java
+++ monitrack-commons/src/main/java/com/monitrack/enumeration/SensorType.java
@@ -1,27 +1,49 @@
 package com.monitrack.enumeration;
 
+
 public enum SensorType {
 	
-	SMOKE("somoke", "fumée"),
-	
-	INPUT_OUTPUT("input/output", "entrée/sortie"),
-	
-	FLOW("flow", "présence");
+	//FIXME - Cheikna : Why do not I remove the english label and make a "this.toString().toLowerCase()" in the constructor ?
+	SMOKE("Smoke", "Fumée", false),	
+	FLOW("Flow", "Présence", false),	
+	DOOR("Door", "Porte", false),	
+	TEMPERATURE("Temperature", "Température", false),	
+	WINDOW("Window", "Fenêtre", false),	
+	HUMIDITY("Humidity", "Humidité", false),	
+	LIGHT("Light", "Lumière", false),
+	GAS("Gas", "Gaz", false),
+	GLASS_BREAKAGE("Glass breakage", "Bris de vitre", false),
+	ACOUSTIC("Acoustic", "Sonore", false),
+	MANUAL_TRIGGER("Manuel trigger", "Déclencheur d'alarme manuel", true),
+	ACCESS_CONTROL("Access controle", "Contrôle d'accès", true),
+	FLOOD("Flood", "Inondation", false);
 	
 	
 	private String englishLabel;
 	private String frenchLabel;	
+	private boolean isManual;
 	
 	/**
 	 * @param englishLabel
 	 * @param frenchLabel
 	 */
-	SensorType(String englishLabel, String frenchLabel) {
+	SensorType(String englishLabel, String frenchLabel, boolean isManual) {
 		this.englishLabel = englishLabel;
 		this.frenchLabel = frenchLabel;
+		this.isManual = isManual;
 	}
 
-
+	public static SensorType getSensorType(String sensorType)
+	{
+		SensorType[] values = SensorType.values();
+		for(SensorType value : values)
+		{
+			if(value.toString().equalsIgnoreCase(sensorType))
+				return value;
+		}
+		return null;
+	}
+	
 	/**
 	 * @return the englishLabel
 	 */
@@ -36,5 +58,8 @@ public enum SensorType {
 	public String getFrenchLabel() {
 		return frenchLabel;
 	}
-	
+
+	public boolean isManual() {
+		return isManual;
+	}
 }
