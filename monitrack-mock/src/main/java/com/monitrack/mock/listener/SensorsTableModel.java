f0a929f070e34774f4ebbb3bb8055bbb386676d7
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-05-06 15:00:21 +0200

    Modification de la TableModel pour mock

--- monitrack-mock/src/main/java/com/monitrack/mock/listener/SensorsTableModel.java
+++ monitrack-mock/src/main/java/com/monitrack/mock/listener/SensorsTableModel.java
@@ -19,10 +19,12 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 
 import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.monitrack.entity.Location;
 import com.monitrack.entity.SensorConfiguration;
 import com.monitrack.enumeration.RequestType;
+import com.monitrack.enumeration.SensorState;
 import com.monitrack.mock.panel.SensorsPage;
 import com.monitrack.mock.runnable.SensorSignal;
 import com.monitrack.shared.MonitrackGuiUtil;
@@ -30,11 +32,12 @@ import com.monitrack.util.JsonUtil;
 
 public class SensorsTableModel extends AbstractTableModel implements ListSelectionListener, ActionListener, DocumentListener {
 
+	private static final Logger log = LoggerFactory.getLogger(SensorsTableModel.class);
 
-	private JTable parent;
+	private JTable parentTable;
 	private SensorsPage sensorsPage;
 	private static final long serialVersionUID = 1L;
-	private String[] header = {"ID", "TYPE", "ACTIVITY", "LOCATION", "MAC ADDRESS", "CURRENT THRESHOLD", "THRESHOLD MIN", "THRESHOLD MAX", "STATE"};
+	private String[] header = {"ID", "TYPE", "ACTIVITY", "LOCATION ID", "MAC ADDRESS", "CURRENT THRESHOLD", "THRESHOLD MIN", "THRESHOLD MAX", "STATE"};
 	private String [][] datas;
 	private String [][] emptyDatas;
 	private final int numberOfRows = 500;
@@ -45,13 +48,16 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 	private Map<Integer, SensorSignal> sensorSignalMap;
 	private List<SensorConfiguration> filteredSensors;
 	private float currentThresholdValue = 0;
+	private int lastRowSelected = 0;
+	private SensorConfiguration lastSensorSelected = null;
+	private int lastIdSent = 0;
 
 
 	public SensorsTableModel(SensorsPage sensorsPage, JTable parent) {
 		this.sensorsPage = sensorsPage;
-		this.parent = parent;
+		this.parentTable = parent;
 		parent.getSelectionModel().addListSelectionListener(this);
-		parent.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		parent.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		parent.setModel(this);
 		datas = new String[numberOfRows][numberOfColumns];
 		emptyDatas = new String[numberOfRows][numberOfColumns];
@@ -61,7 +67,8 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 	}
 
 	@SuppressWarnings("unchecked")
-	private void loadDatas() {
+	private void loadDatabaseDatas() {
+		datas = emptyDatas.clone();
 		try {
 			String jsonRequest = JsonUtil.serializeRequest(RequestType.SELECT, SensorConfiguration.class, null, null, null, null);
 			String response = MonitrackGuiUtil.sendRequest(jsonRequest);
@@ -70,13 +77,34 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 			if(sensors != null) {
 				int size = sensors.size();
 				for(int i = 0; i < size; i++) {
-					initSensorConfigurationDatas(sensors.get(i));					
+					initSensorConfigurationDatas(sensors.get(i), "");					
 				}				
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e.getMessage());
 		}
-
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void loadCacheDatas() {
+		datas = emptyDatas.clone();
+		SensorState[] states = SensorState.values();
+		try {
+			String jsonRequest = null;
+			for(SensorState state : states) {
+				String stateName = state.name();
+				jsonRequest = JsonUtil.serializeSensorsUpdateRequest(state);
+				String response = MonitrackGuiUtil.sendRequest(jsonRequest);
+				List<SensorConfiguration> sensors = (List<SensorConfiguration>)JsonUtil.deserializeObject(response);
+				for(SensorConfiguration sensor : sensors) {		
+					initSensorConfigurationDatas(sensor, stateName);
+				}
+				
+			}
+			
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
 	}
 
 	@Override
@@ -110,19 +138,19 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 		return header[column];
 	}
 
-	public void initSensorConfigurationDatas(SensorConfiguration sensorConfiguration) {
+	public void initSensorConfigurationDatas(SensorConfiguration sensorConfiguration, String state) {
 		String id = sensorConfiguration.getSensorConfigurationId().toString();
 		String type = sensorConfiguration.getSensorType().getEnglishLabel();
 		String activity = sensorConfiguration.getSensorActivity().name();
-		String location = "";
+		String locationId = sensorConfiguration.getLocationId().toString();
 		String macAddress = sensorConfiguration.getMacAddress();
 		String currentThreshold = sensorConfiguration.getCurrentThreshold().toString();
 		String minThreshold = sensorConfiguration.getMinDangerThreshold().toString();
 		String maxThreshold = sensorConfiguration.getMaxDangerThreshold().toString();
-		datas[firstEmptyIndex] = new String[]{id, type, activity, location, 
+		datas[firstEmptyIndex] = new String[]{id, type, activity, locationId, 
 				macAddress, currentThreshold, 
 				minThreshold, maxThreshold, 
-		"STATE"};
+		state};
 		firstEmptyIndex++;
 		updateTable(); 
 	}
@@ -132,28 +160,21 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 		fireTableDataChanged();
 	}
 
-	public List<SensorConfiguration> getSensors() {
-		return sensors;
-	}
-
-	public void setSensors(List<SensorConfiguration> sensors) {
-		this.sensors = sensors;
-	}
-
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		if(!e.getValueIsAdjusting()) {
-			int[] rows =  parent.getSelectedRows();
-			for(int i : rows) {
-				//FIXME
-				System.out.println(i);
+			lastRowSelected = parentTable.getSelectedRow();
+			int filteredSize = filteredSensors.size();
+			if(lastRowSelected >= 0 && lastRowSelected < filteredSize) {
+				lastSensorSelected = filteredSensors.get(lastRowSelected);
 			}
+			sensorsPage.getRateValueLabel().setText("Current threshold : " + currentThresholdValue 
+					+ " " + lastSensorSelected.getMeasurementUnit() + " (last id n°" + lastIdSent + ")");
 		}
-
 	}
 
 	public void setParent(JTable parent) {
-		this.parent = parent;
+		this.parentTable = parent;
 	}
 
 	@Override
@@ -171,7 +192,7 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 				for(int i = 0 ; i < size; i++) {
 					SensorConfiguration sensor = sensors.get(i);	
 					if(isSensorCorrect(sensor)) {
-						initSensorConfigurationDatas(sensor);
+						initSensorConfigurationDatas(sensor, "");
 						filteredSensors.add(sensor);
 					}				
 				}
@@ -182,10 +203,12 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 		}
 		else if(e.getSource() == sensorsPage.getStopSendingMessageButton()) {
 			startStopSensorSignal(false);
-
 		}
-		else if(e.getSource() == sensorsPage.getLoadDatasButton()) {
-			loadDatas();			
+		else if(e.getSource() == sensorsPage.getLoadDatasFromDatabaseButton()) {
+			loadDatabaseDatas();			
+		}
+		else if(e.getSource() == sensorsPage.getLoadDatasFromServerCacheButton()) {
+			loadCacheDatas();			
 		}
 
 		updateTable();
@@ -195,15 +218,16 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 		String idString = sensorsPage.getIdTextField().getText().trim();
 		if(idString.length() > 0 && NumberUtils.toInt(idString) != sensor.getSensorConfigurationId())
 			return false;
-		if(!sensorsPage.getSensorTypeComboBox().getSelectedItem().toString().equalsIgnoreCase(sensor.getSensorType().name()))
+		String type = sensorsPage.getSensorTypeComboBox().getSelectedItem().toString();
+		if(!type.equalsIgnoreCase("-") && !type.equalsIgnoreCase(sensor.getSensorType().name()))
 			return false;
-		if(!sensorsPage.getSensorActivityComboBox().getSelectedItem().toString().equalsIgnoreCase(sensor.getSensorActivity().name()))
+		
+		String activity = sensorsPage.getSensorActivityComboBox().getSelectedItem().toString();
+		if(!activity.equalsIgnoreCase("-") && !activity.equalsIgnoreCase(sensor.getSensorActivity().name()))
 			return false;
 
-		String locationFilter = sensorsPage.getLocationTextField().getText().trim();
-		Location location = sensor.getLocation();
-		String locationName = (location != null) ? location.getNameLocation() : "";
-		if(!locationFilter.equalsIgnoreCase(locationName))
+		String locationIdFilter = sensorsPage.getLocationTextField().getText().trim();
+		if(locationIdFilter.length() > 0 && !locationIdFilter.equalsIgnoreCase(sensor.getLocationId().toString()))
 			return false;
 
 		return true;
@@ -235,37 +259,36 @@ public class SensorsTableModel extends AbstractTableModel implements ListSelecti
 				field.setBackground(Color.ORANGE);			
 			} else {
 				field.setBackground(Color.WHITE);	
-				sensorsPage.getRateValueLabel().setText("Current threshold : " + value);
 				currentThresholdValue = value;
+				String unit = "";
+				if(lastSensorSelected != null)
+					unit = lastSensorSelected.getMeasurementUnit();
+				sensorsPage.getRateValueLabel().setText("Current threshold : " + currentThresholdValue + " " + unit 
+				+ " (last id n°" + lastIdSent + ")");
 
 			}
 		}
 	}
 
 	private void startStopSensorSignal(boolean sendSignal) {
-		int[] rows =  parent.getSelectedRows();
-		int filteredSize = filteredSensors.size();
-		for(int i : rows) {
-			if(i >= 0 && i < filteredSize) {
-				SensorConfiguration sensor = filteredSensors.get(i);
-				int id = sensor.getSensorConfigurationId();
-				SensorSignal signal = null;
-				if(sensorSignalMap.containsKey(sensor.getSensorConfigurationId())){
-					signal = sensorSignalMap.get(id);
-					signal.getSensorConfiguration().setCurrentThreshold(currentThresholdValue);
-					signal.setSendMessage(sendSignal);
-				}
-				else {
-					sensor.setCurrentThreshold(currentThresholdValue);
-					signal = new SensorSignal(sensor);	
-					signal.setSendMessage(sendSignal);		
-					sensorSignalMap.put(id, signal);
-					Thread thread = new Thread(signal);
-					thread.start();
-				}
+		if(lastSensorSelected != null) {
+			int id = lastSensorSelected.getSensorConfigurationId();
+			lastIdSent = id;
+			SensorSignal signal = null;
+			if(sensorSignalMap.containsKey(lastSensorSelected.getSensorConfigurationId())){
+				signal = sensorSignalMap.get(id);
+				signal.setThresholdReached(currentThresholdValue);
+				signal.setSendMessage(sendSignal);
+			}
+			else {
+				lastSensorSelected.setCurrentThreshold(currentThresholdValue);
+				signal = new SensorSignal(id, currentThresholdValue, lastSensorSelected.getCheckFrequency().longValue());	
+				signal.setSendMessage(sendSignal);		
+				sensorSignalMap.put(id, signal);
+				Thread thread = new Thread(signal);
+				thread.start();
 			}
 		}
-		
 
 	}
 
